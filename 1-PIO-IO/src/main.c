85270a4ea202db5b3f3b9e16b601cfe7d0d6f2e5
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2023-02-23 10:36:45 -0300

    LAB 1

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -1,75 +1,102 @@
-/**
- * 5 semestre - Eng. da Computação - Insper
- * Rafael Corsi - rafael.corsi@insper.edu.br
- *
- * Projeto 0 para a placa SAME70-XPLD
- *
- * Objetivo :
- *  - Introduzir ASF e HAL
- *  - Configuracao de clock
- *  - Configuracao pino In/Out
- *
- * Material :
- *  - Kit: ATMEL SAME70-XPLD - ARM CORTEX M7
- */
-
-/************************************************************************/
-/* includes                                                             */
-/************************************************************************/
-
 #include "asf.h"
 
-/************************************************************************/
-/* defines                                                              */
-/************************************************************************/
+#define BUT1_PIO PIOD
+#define BUT1_ID ID_PIOD
+#define BUT1_IDX 28
+#define BUT1_MASK (1 << BUT1_IDX)
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+#define BUT2_PIO PIOC
+#define BUT2_ID ID_PIOC
+#define BUT2_IDX 31
+#define BUT2_MASK (1 << BUT2_IDX)
 
-/************************************************************************/
-/* constants                                                            */
-/************************************************************************/
+#define BUT3_PIO PIOA
+#define BUT3_ID ID_PIOA
+#define BUT3_IDX 19
+#define BUT3_MASK (1 << BUT3_IDX)
 
-/************************************************************************/
-/* variaveis globais                                                    */
-/************************************************************************/
+#define LED1_PIO PIOA
+#define LED1_ID ID_PIOA
+#define LED1_IDX 0
+#define LED1_MASK (1 << LED1_IDX)
 
-/************************************************************************/
-/* prototypes                                                           */
-/************************************************************************/
+#define LED2_PIO PIOC
+#define LED2_ID ID_PIOC
+#define LED2_IDX 30
+#define LED2_MASK (1 << LED2_IDX)
 
-void init(void);
+#define LED3_PIO PIOB
+#define LED3_ID ID_PIOB
+#define LED3_IDX 2
+#define LED3_MASK (1 << LED3_IDX)
 
-/************************************************************************/
-/* interrupcoes                                                         */
-/************************************************************************/
-
-/************************************************************************/
-/* funcoes                                                              */
-/************************************************************************/
-
-// Função de inicialização do uC
 void init(void)
 {
+	sysclk_init();											// INIT Board Clock
+	WDT->WDT_MR = WDT_MR_WDDIS;								// Desativa WatchDog Timer
 
+	//Led 1
+	pmc_enable_periph_clk(LED1_ID);						// Ativa o PIO utilizando ID, Modo de Entrada
+	pio_set_output(LED1_PIO, LED1_MASK, 0, 0, 0);		// Configura PIOC 8 como saida
+	//Led 2
+	pmc_enable_periph_clk(LED2_ID);
+	pio_set_output(LED2_PIO, LED2_MASK, 0, 0, 0);
+	//Led 3
+	pmc_enable_periph_clk(LED3_ID);
+	pio_set_output(LED3_PIO, LED3_MASK, 0, 0, 0);
+	//Button 1
+	pmc_enable_periph_clk(BUT1_ID);								// Ativando Botao 1
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_MASK, PIO_PULLUP);	// Configurando Botao 1
+	// Button 2
+	pmc_enable_periph_clk(BUT2_ID);
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT2_MASK, PIO_PULLUP);
+	// Button 3
+	pmc_enable_periph_clk(BUT3_ID);
+	pio_configure(BUT3_PIO, PIO_INPUT, BUT3_MASK, PIO_PULLUP);
 }
 
-/************************************************************************/
-/* Main                                                                 */
-/************************************************************************/
-
-// Funcao principal chamada na inicalizacao do uC.
 int main(void)
 {
-  init();
+	init();
 
-  // super loop
-  // aplicacoes embarcadas não devem sair do while(1).
-  while (1)
-  {
-
-  }
-  return 0;
+	while (1)
+	{
+		if (!pio_get(BUT1_PIO, PIO_INPUT, BUT1_MASK)){
+			for(int i = 0; i < 5; i++){
+				pio_clear(LED1_PIO, LED1_MASK);
+				delay_ms(100);
+				pio_set(LED1_PIO, LED1_MASK);
+				delay_ms(100);
+			}
+		}
+		else
+		{
+			pio_set(LED1_PIO, LED1_MASK);
+		}
+		if (!pio_get(BUT2_PIO, PIO_INPUT, BUT2_MASK)){
+			for(int i = 0; i < 5; i++){
+				pio_clear(LED2_PIO, LED2_MASK);
+				delay_ms(100);
+				pio_set(LED2_PIO, LED2_MASK);
+				delay_ms(100);
+			}
+		}
+		else
+		{
+			pio_set(LED2_PIO, LED2_MASK);
+		}
+		if (!pio_get(BUT3_PIO, PIO_INPUT, BUT3_MASK)){
+			for(int i = 0; i < 5; i++){
+				pio_clear(LED3_PIO, LED3_MASK);
+				delay_ms(100);
+				pio_set(LED3_PIO, LED3_MASK);
+				delay_ms(100);
+			}
+		}
+		else
+		{
+			pio_set(LED3_PIO, LED3_MASK);
+		}
+	}
+	return 0;
 }
